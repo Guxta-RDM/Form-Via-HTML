cd6b2c9dbc0f726f5f43555fd5646da62fffb4b9
Guxta RDM <149907373+Guxta-RDM@users.noreply.github.com>
2023-11-10 13:47:48 -0300

    Raiz feita

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const app = express();
 //Indicando para a aplicação como servir arquivos estáticos 
 app.use(express.static('./Paginas'));
 
+app.get('/', (requisicao, resposta) => {
+    resposta.end(`
+    <!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Menu do Sistema</title>
+    </head>
+    <body>
+        <h1>MENU</h1>
+        <ul>
+            <li><a href="/cadastro.html">Cadastro</a></li>
+        </ul>
+    </body>
+    </html>
+    `)
+})
+
 app.listen(porta, host,() => {
-    console.log('Servidor rodando na url http://$(host):$(porta)')
+    console.log(`Servidor rodando na url http://${host}:${porta}`);
 });
